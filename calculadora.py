c26bfa7f0ef73aeab7c8bbe4b3872d26ec9708df
Bradog <fradyzz@hotmail.com>
2020-02-28 23:50:08 -0500

    Métodos y bases de la calculadora
    Cree los métodos y las bases iniciales para la calculadora, es necesario qeu cada grupo escoja una función que desarrollará y cree un new pull request

---
+++ calculadora.py
@@ -0,0 +1,11 @@
+pm=***********Bienvenido a la Calculadora***********; print(pm);
+def suma(a,b):
+   pass;
+def resta(a, b):
+   pass;
+def multiplicacion(a, b):
+   pass;
+def division(a, b):
+   pass;
+def potencia (a):
+   pass;
